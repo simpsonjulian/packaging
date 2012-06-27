5f84096c5dcf79eacfbd05513280299b20f9169d
Julian Simpson <simpsonjulian@gmail.com>
2012-06-27 16:24:33 +0100

    Almost there.

--- installer-qa/features/basic_qa_test.feature
+++ installer-qa/features/basic_qa_test.feature
@@ -5,12 +5,12 @@ Feature: Install Neo4j Tarball on Linux
   So I don't go and try another database
 
   Scenario Outline: Install Different Editions
-    Given I have a brand new virtual machine
-    When I copy the tarball for the <edition> edition to the <vm> virtual machine
-    And I untar the tarball for the <edition> edition on the <vm> virtual machine
-    And I uncomment the 'org.neo4j.server.webserver.address' option in the 'neo4j-server.properties' config file for the <edition> edition on the <vm> virtual machine
-    And I start the neo4j script for the <edition> edition on the <vm> virtual machine
-    Then I should see the webadmin page over HTTP for machine <vm>
+    Given I have a brand new <vm> VM
+    When I copy the tarball for the <edition> edition to the <vm> VM
+    And I untar the tarball for the <edition> edition on the <vm> VM
+    And I uncomment the 'org.neo4j.server.webserver.address' option in the 'neo4j-server.properties' config file for the <edition> edition on the <vm> VM
+    And I start the neo4j script for the <edition> edition on the <vm> VM
+    Then I should see the webadmin page over HTTP for VM <vm>
 
   Examples:
     | edition    |  vm      |
--- installer-qa/features/step_definitions/steps.rb
+++ installer-qa/features/step_definitions/steps.rb
@@ -18,42 +18,44 @@ def command(command_line, machine)
 end
 
 
-Given /^I have a brand new virtual machine$/ do
+Given /^I have a brand new (.*) VM$/ do |vm|
   @env = Vagrant::Environment.new(:ui_class => Vagrant::UI::Basic)
-  @env.cli("destroy","-f")
+  @env.cli("destroy","-f", vm)
   @env.cli("up")
-  puts "Virtual Machines all new and interesting"
+  puts "VM #{vm} is new and interesting"
 end
 
-When /^I copy the tarball for the (.*) edition to the (.*) virtual machine$/ do |edition, vm|
+When /^I copy the tarball for the (.*) edition to the (.*) VM$/ do |edition, vm|
   index = index(vm)
-  @env.vms_ordered[index].channel.upload("../standalone/target/neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz","/home/vagrant/.")
+  vm = @env.vms_ordered[index]
+  puts vm.inspect
+  vm.channel.upload("../standalone/target/neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz","/home/vagrant/.")
 end
 
 
 
-When /^I untar the tarball for the (.*) edition on the (.*) virtual machine$/ do |edition, vm|
+When /^I untar the tarball for the (.*) edition on the (.*) VM$/ do |edition, vm|
   command("tar -xf neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz", vm)
 end
 
-When /^I uncomment the '(.*)' option in the '(.*)' config file for the (.*) edition on the (.*) machine$/ do |option, file, edition, vm|
+When /^I uncomment the '(.*)' option in the '(.*)' config file for the (.*) edition on the (.*) VM/ do |option, file, edition, vm|
   command = "sed -i 's/^##{option.sub('.','\.')}/#{option}/' neo4j-#{edition}-1.8-SNAPSHOT/conf/#{file}"
   command(command, vm)
 end
 
-When /^I (.*) the (.*) script for the (.*) edition on the (.*) machine$/ do |action, script, edition, vm|
+When /^I (.*) the (.*) script for the (.*) edition on the (.*) VM/ do |action, script, edition, vm|
 
   command "./neo4j-#{edition}-1.8-SNAPSHOT/bin/#{script} #{action}", vm
 
 end
 
-Then /^I should see the webadmin page over HTTP for machine (.*)$/ do  |vm|
+Then /^I should see the webadmin page over HTTP for VM (.*)$/ do  |vm|
 
   sleep 10 # we need the webdriver wait method
 
   # this sickens me but I can't see how to do this via the API
 
-  ip_address = File.read('Vagrantfile').each_line.select{|line| line =~ /network/ }[index].last.split(' ').last.gsub(/"/, '')
+  ip_address = File.read('Vagrantfile').each_line.select{|line| line =~ /network/ }[index(vm)].split(' ').last.gsub(/"/, '')
   Capybara.app_host = "http://#{ip_address}:7474"
 
   #url = "http://#{ip_address}:7474/webadmin"
