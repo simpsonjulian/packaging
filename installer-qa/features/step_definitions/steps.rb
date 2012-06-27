02d8c10be28ab6c4ec476844253ce219528f3bda
Julian Simpson <simpsonjulian@gmail.com>
2012-06-27 18:55:25 +0100

    Working.

--- installer-qa/features/step_definitions/steps.rb
+++ installer-qa/features/step_definitions/steps.rb
@@ -7,7 +7,7 @@ include FileUtils
 
 
 def index(vm)
-  vm[6].to_i - 1
+  vm[5..6].to_i - 1
 end
 
 def command(command_line, machine)
@@ -22,17 +22,13 @@ Given /^I have a brand new (.*) VM$/ do |vm|
   @env = Vagrant::Environment.new(:ui_class => Vagrant::UI::Basic)
   @env.cli("destroy","-f", vm)
   @env.cli("up")
-  puts "VM #{vm} is new and interesting"
 end
 
 When /^I copy the tarball for the (.*) edition to the (.*) VM$/ do |edition, vm|
   index = index(vm)
   vm = @env.vms_ordered[index]
-  puts vm.inspect
-  vm.channel.upload("../standalone/target/neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz","/home/vagrant/.")
+  puts vm.channel.upload("../standalone/target/neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz","/home/vagrant/.")
 end
-
-
 
 When /^I untar the tarball for the (.*) edition on the (.*) VM$/ do |edition, vm|
   command("tar -xf neo4j-#{edition}-1.8-SNAPSHOT-unix.tar.gz", vm)
@@ -53,7 +49,7 @@ Then /^I should see the webadmin page over HTTP for VM (.*)$/ do  |vm|
 
   sleep 10 # we need the webdriver wait method
 
-  # this sickens me but I can't see how to do this via the API
+  # this sickens me but I can't yet see how to do this via the API
 
   ip_address = File.read('Vagrantfile').each_line.select{|line| line =~ /network/ }[index(vm)].split(' ').last.gsub(/"/, '')
   Capybara.app_host = "http://#{ip_address}:7474"
